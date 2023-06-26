baf4d9d8545d8603b23fc2bc6391d779408eb6cc
SCYangcode <97731569+SCYangcode@users.noreply.github.com>
2023-06-26 22:15:26 +0800

    Update trim_miRNA_adapter.sh

--- trim_miRNA_adapter.sh
+++ trim_miRNA_adapter.sh
@@ -1,27 +1,28 @@
-#!/bin/bash
 echo "please enter the sequence of five_prime_adapter"
 read five_prime_adapter
 echo "please enter the sequence of three_prime_adapter"
 read three_prime_adapter
 echo "please enter the sequence of Index_Primers"
 read Index_Primers
+echo "please enter the name of fastq"
+read fastq
 
-cutadapt -a $five_prime_adapter -n 1 -e 0.2 -O 5 -m 1 --match-read-wildcards -o tmp_Cutadapt3.fastq SRR837839.fastq 
+cutadapt -a $five_prime_adapter -n 1 -e 0.2 -O 5 -m 1 --match-read-wildcards -o tmp_Cutadapt3.fastq $fastq
 
 cutadapt -g $three_prime_adapter -n 1 -e 0.125 -O 8 -m 1 --match-read-wildcards -o tmp_Cutadapt2.fastq tmp_Cutadapt3.fastq
 
 cutadapt -b $five_prime_adapter -b $three_prime_adapter -n 1 -e 0.0625 -O 12 -m 1 --match-read-wildcards --discard-trimmed -o tmp_Cutadapt1.fastq tmp_Cutadapt2.fastq
 
-cutadapt -b $ Index_Primers -n 1 -e 0.000 -O 23 --no-trim --untrimmed-output SRR837839_Cutadapt.fastq -o tmp_Cutadapt_rm.fastq tmp_Cutadapt.fastq
+cutadapt -b $Index_Primers -n 1 -e 0.0625 -O 15 --no-trim --untrimmed-output purpose_Cutadapt.fastq -o tmp_Cutadapt.fastq tmp_Cutadapt1.fastq
 
-fastq_quality_filter -q 20 -p 95 -i SRR837839_Cutadapt.fastq -o SRR837839_filted.fastq
+fastq_quality_filter -q 20 -p 95 -i purpose_Cutadapt.fastq -o purpose_filted.fastq
 
-fastx_collapser -i SRR837839_filted.fastq -o SRR837839_Collapsed.fa
+fastx_collapser -i purpose_filted.fastq -o purpose_Collapsed.fa
 
-cutadapt -b CGTTCCCGTGG -b CCACGTTCCCG -n 1 -e 0.000 -O 11 --no-trim --untrimmed-output stp0_Processed.fa -o SRR837839_stp_mis_0.fa SRR837839_Collapsed.fa
- 
-cutadapt -b CGTTCCCGTGG -n 1 -e 0.091 -O 11 --no-trim --untrimmed-output stp1_Processed.fa -o SRR837839_stp_mis_1.fa stp0_Processed.fa
- 
-cutadapt -b CGTTCCCGTGG -n 1 -e 0.182 -O 11 --no-trim --untrimmed-output SRR837839_Processed.fa -o SRR837839_stp_mis_2.fa stp1_Processed.fa
+cutadapt -b CGTTCCCGTGG -b CCACGTTCCCG -n 1 -e 0.000 -O 11 --no-trim --untrimmed-output stp0_Processed.fa -o purpose_stp_mis_0.fa purpose_Collapsed.fa
 
-rm SRR837839_stp_mis_2.fa SRR837839_stp_mis_1.fa SRR837839_stp_mis_0.fa stp1_Processed.fa stp0_Processed.fa SRR837839_Collapsed.fa SRR837839_filted.fastq tmp_Cutadapt_rm.fastq tmp_Cutadapt3.fastq tmp_Cutadapt2.fastq tmp_Cutadapt1.fastq tmp_Cutadapt.fastq
+cutadapt -b CGTTCCCGTGG -n 1 -e 0.091 -O 11 --no-trim --untrimmed-output stp1_Processed.fa -o purpose_stp_mis_1.fa stp0_Processed.fa
+
+cutadapt -b CGTTCCCGTGG -n 1 -e 0.182 -O 11 --no-trim --untrimmed-output purpose_Processed.fa -o purpose_stp_mis_2.fa stp1_Processed.fa
+
+rm purpose_stp_mis_2.fa purpose_stp_mis_1.fa purpose_stp_mis_0.fa stp1_Processed.fa stp0_Processed.fa purpose_Collapsed.fa purpose_filted.fastq tmp_Cutadapt.fastq tmp_Cutadapt3.fastq tmp_Cutadapt2.fastq tmp_Cutadapt1.fastq purpose_Cutadapt.fastq
